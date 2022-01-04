7926f4e7bff979a87cea07e7e519f81c3e7e15ab
maivananh2k02 <maihuyen2k02@gmail.com>
2022-01-04 15:16:08 +0700

    update walletKeplr

--- src/components/sidebar/TabSideBar.vue
+++ src/components/sidebar/TabSideBar.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
-        <li v-for="(tab,index) in tabs" :key="index"><a :href="tab.url">
-            <div class="icon"><img class="default" :src="tab.image_default" alt="index"/><img
-                class="active" :src="tab.image_action" alt="index"/></div>
-            <span class="title">{{ tab.name }}</span></a></li>
+        <li v-for="(tab,index) in tabs" :key="index" :class="showClass(tab.url)" @click="checkClass(tab.url)">
+            <router-link :to="tab.url">
+                <div class="icon"><img class="default" :src="tab.image_default" alt="index"/><img
+                    class="active" :src="tab.image_action" alt="index"/></div>
+                <span class="title">{{ tab.name }}</span></router-link>
+        </li>
     </div>
 
 </template>
@@ -15,7 +17,21 @@ export default {
     name: "TabSideBar",
     data: function () {
         return {
-            tabs: defaultData
+            tabs: defaultData,
+        }
+    },
+    props:{
+      check:String
+    },
+    methods: {
+        checkClass(path) {
+            this.check = path
+        },
+        showClass(path) {
+            if (this.check === path) {
+                return 'menu-active'
+            }
+            return ''
         }
     }
 }
