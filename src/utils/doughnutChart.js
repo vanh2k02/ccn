11b08c60fa24a48482ffea8970625f1193ff39d4
HoangPham <hoangpa@paditech.com>
2022-01-12 16:07:41 +0700

    Update refresh chart

--- src/utils/doughnutChart.js
+++ src/utils/doughnutChart.js
@@ -1,7 +1,13 @@
-import { Doughnut } from "vue-chartjs";
+import { Doughnut, mixins } from "vue-chartjs";
 export default {
     extends: Doughnut,
     props: ["data"],
+    mixins: [mixins.reactiveProp],
+    watch: {
+        data () {
+          this.renderChart(this.data)
+        }
+    },
     mounted() {
         this.renderChart(this.data)
     }
